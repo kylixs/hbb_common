b212885a467844db6d4b43d2c75ef917de7e07fb
Gong Dewei <kylixs@qq.com>
2025-10-18 01:55:27 +0800

    change default server

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk-server.jlcops.com"];
+pub const RS_PUB_KEY: &str = "dH4eiv9iaKF04PYI1NroZHzN53KtFaTgzGzfpTbuPhA=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
